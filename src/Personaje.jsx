00d585d3ad227c3468b1c9087e8bfa7e5ede7a42
JaimeMottaSena <13alejandromottamesa@gmail.com>
2021-09-11 21:18:05 -0500

    Taller3PersonajesImg

---
+++ src/Personaje.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Personaje = ({nombre, edad, especialidad,nacionalidad, id,foto}) => (
+    <>
+        <div className="card col-md-4" >
+            <div className="card-body">
+                <h5 className="card-title">{ nombre }</h5>
+                <p className="card-text">Edad: { edad }</p>
+                <p className="card-text">Especialidad: { especialidad }</p>
+               
+                <p className="card-text">Nacionalidad: { nacionalidad }</p>
+                <p className="card-image">Foto: {  }</p>
+                <img src={foto}/>
+                <p><a href={`/personajes/${id}`}>Detalle</a></p>
+            </div>
+        </div>
+    </>
+);
+Personaje.propTypes = {
+    nombre: PropTypes.string,
+    edad: PropTypes.number,
+    especialidad: PropTypes.string,
+    nacionalidad: PropTypes.string,
+   
+  
+
+}
+Personaje.defaultProps = {
+    nombre: "nombre no asignado",
+    edad: 0
+}
+export default Personaje;
